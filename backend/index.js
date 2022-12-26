074a78bec582ead949c361fee1539a6fcba96e3e
karan <ashiwalkaran@gmail.com>
2022-12-26 17:01:34 +0530

    models and routes and database schema designing.

--- backend/index.js
+++ backend/index.js
@@ -8,9 +8,8 @@ const port=5000;
 
 //Available routes
 
-app.get('/',(req,res)=>{
-    res.send('Hello World!!!!!');
-});
+app.use('/api/auth',require('./routes/auth'));
+// app.use('/api/notes',require('./routes/notes'));
 
 app.listen(port,()=>{
     console.log(`Listening on port ${port}`);
