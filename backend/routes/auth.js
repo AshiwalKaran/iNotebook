66c5e70a66590eed9950ec6dc2c0fb8e016f0233
karan <ashiwalkaran@gmail.com>
2022-12-27 00:09:42 +0530

    5th--> custom message for duplicate email and modifying the '/' url to '/createUser' url and collection of thunderclient requests

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -10,30 +10,39 @@ const { body, validationResult } = require('express-validator');
 //     res.send(req.body);
 // });
 
-router.post('/',[
+router.post('/createUser',[
     body('name','Enter a valid name').isLength({min:3}),
     body('email','Enter a valid email').isEmail(),
     body('password','Password must be atleast 5 characters').isLength({min:5})
     ],
-    (req,res)=>{
+    async (req,res)=>{
+        //If there are errors ,return Bad request and the errors.
         const errors=validationResult(req);
         if(!errors.isEmpty()){
             return res.status(400).json({errors:errors.array()});
         }
-        console.log(req.body);
+        // console.log(req.body);
+
+        //checking if the user with this email already exists or not
+        let user=await User.findOne({email:req.body.email});
+        if(user){
+            return res.status(400).json({error:"Sorry a user with this email already exists"});
+        }
+
         // const user=User(req.body);
         // user.save();
         // res.send(req.body);
 
         //New way to create a user instead of user.save() function.
-        User.create({
+        user =await User.create({
             name:req.body.name,
             email:req.body.email,
             password:req.body.password
         })
-        .then(user=>res.json(user))
-        .catch(err=>{console.log(err)
-        res.json({error:'Please enter a unique value for email',message:err.message})});
+        // .then(user=>res.json(user))
+        // .catch(err=>{console.log(err)
+        // res.json({error:'Please enter a unique value for email',message:err.message})});
+        res.json(user);
     });
 
 module.exports=router;
