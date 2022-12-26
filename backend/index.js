52329128fd1e8ef245d854f57108d31ec83b13cd
karan <ashiwalkaran@gmail.com>
2022-12-26 17:29:49 +0530

    app.use() middleware to use req.body and saving the user in User model

--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,9 @@ connectToMongo();
 const app=express();
 const port=5000;
 
+//To access the body of request we need to add "app.use(express.json())" middleware
+app.use(express.json());
+
 //Available routes
 
 app.use('/api/auth',require('./routes/auth'));
